69d2ec7fa0cf7bb367fd1f24d3508df5f100e029
Roland RAKOTOMALALA <rolrkt@yahoo.com>
2025-09-15 11:45:51 +0200

    main.py = 10/10

--- src/greeting/main.py
+++ src/greeting/main.py
@@ -1,4 +1,15 @@
+"""_summary_
+"""
+
 def greet(name):
+    """_summary_
+
+    Args:
+        name (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return f"Hello, {name}!"
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,12 @@ from greeting.utils import add
 
 
 def test_greet():
+    """_summary_
+    """    
     assert greet("World") == "Hello, World!"
 
 
 def test_add():
+    """_summary_
+    """    
     assert add(5, 7) == 12
